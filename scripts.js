3658f0b3b771c60127980ead6428522128052b8b
nickimpoco <nickimpoco@gmail.com>
2022-12-08 16:14:16 -0500

    Stopping to work on ch23

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100){
+    if (window.pageYOffset > 100) {
         toTop.classList.add("active");
-    }else{
+    } else {
         toTop.classList.remove("active");
     }
+})
+
+const redirectNavLogo = document.getElementById("redirectNavExampleID");
+redirectNavLogo.addEventListener("click", function(){
+     let response = window.confirm("Are you sure you want to be redirected away from this page?");
+     if (response) {
+          
+     } else{
+
+     }
 })
